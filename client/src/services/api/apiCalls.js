69255892702c62e60db722fb825f0e4044510917
Mohamed Shaheer <shaheer@coderythm.com>
2024-07-07 11:23:51 +0400

    feat: added server url in client

--- client/src/services/api/apiCalls.js
+++ client/src/services/api/apiCalls.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:5000";
+// const url = "http://localhost:5000";
+const url = "https://cms-mern-server.onrender.com"
 
 export const registerUser = async ({ email, password }) => {
   try {
